0da9a7800b523b77b1c1d4487a53ee1f4c2f53f2
Peter Adeyemo <adeyemodanointed@yahoo.com>
2021-11-18 06:26:55 +0100

    Models added

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT;
 
+const productRoutes = require('./routes/product')
+const userRoutes = require('./routes/user')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
+
+app.use('/product', productRoutes);
+app.use('/user', userRoutes);
+
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port);
--- models/user.js
+++ models/user.js
@@ -1,27 +1,39 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const userSchema = new Schema({
-//   email: {
-//     type: String,
-//     required: true,
-//     allowNull: false
-//   },
-//   name: {
-//     type: String,
-//     required: true,
-//     allowNull: false
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     allowNull: false
-//   },
-//   cart: {
-//     items: {
-//       type: 
-//     }
-//   }
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    allowNull: false
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    allowNull: false
+  },
+  userRole: {
+    type: Number,
+    required: true,
+    allowNull: false
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        purchaseQuantity: { type: Number, required: true }
+      }
+    ]
+  }
+})
 
-// })
+module.exports = ('User', userSchema);
